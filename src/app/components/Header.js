b8a0c052bdf977e02c00c3e82ed5c76ecb480ed9
jfgarcia0911 <jfgarcia0911@gmail.com>
2025-08-08 23:43:18 +0800

    first commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to the Facebook Clone
+      </h1>
+    </div>
+  )
+}
